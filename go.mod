2ea112ff3f3a3782ea23c1a9435bf4ab33fe3a5d
poylol <poylolt@gmail.com>
2025-02-14 23:41:42 -0800

    Tesseract Included!

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module backend
 go 1.23.5
 
 require (
+	github.com/otiai10/gosseract/v2 v2.4.1 // indirect
+	github.com/warthog618/go-gpiocdev v0.9.1 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/mail.v2 v2.3.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,24 +2,41 @@ package main
 
 import (
 	"fmt"
+	"github.com/otiai10/gosseract/v2"
 	gomail "gopkg.in/mail.v2"
+	"os"
 )
 
 func main() {
+	recieved, er := getTextFromImage("./tests/testImage.png")
+	if er != nil {
+		fmt.Println(er)
+	}
+	sendMail("Pollo", "poylolt@gmail.com", recieved, "This message was read from an image of a document with OCR!!")
+}
+func getTextFromImage(imagePath string) (text string, er error) {
+	client := gosseract.NewClient()
+	defer client.Close()
+	client.SetImage(imagePath)
+	text, er = client.Text()
+	return text, er
+}
+func sendMail(From string, To string, Subject string, msg string) {
 	// message
 	message := gomail.NewMessage()
 
 	//                          This should be an env variable
 	message.SetHeader("From", "mesapidemo@gmail.com")
 	//                      This would be inputted by user
-	message.SetHeader("To", "poylolt@gmail.com")
-	message.SetHeader("Subject", "[From: Name goes here ] This message was sent through a Raspberry Pi*")
+	message.SetHeader("To", To)
+	message.SetHeader("Subject", "[ From: "+From+"] "+Subject)
 
-	message.SetBody("text/plain", "Yey it worked!")
+	message.SetBody("text/plain", msg)
 
 	// I should understand what does this line does I hope it is not much problem
 	//                                               This password should be an environment variable
-	dialer := gomail.NewDialer("smtp.gmail.com", 587, "mesapidemo@gmail.com", "vqzwgroptpdisxwb")
+	password := os.Getenv("GMAILKEY")
+	dialer := gomail.NewDialer("smtp.gmail.com", 587, "mesapidemo@gmail.com", password)
 
 	if err := dialer.DialAndSend(message); err != nil {
 		fmt.Println("Error:", err)
@@ -27,4 +44,5 @@ func main() {
 	} else {
 		fmt.Println("Email sent successfully!")
 	}
+
 }
